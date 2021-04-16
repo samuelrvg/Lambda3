f9348023414d2ee148d48feffc086b37c0e5e965
samuel <samuel.rvg@gmail.com>
2021-04-16 18:20:54 -0400

    Criação da tela principal ja retornando os games da api

---
+++ CopaGames/components/card.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-card :sub-title="game.titulo">
+    <input type="checkbox" :value="game.id" />
+    <label>Selecionar game</label><br />
+    <!-- <b-card-text> </b-card-text>
+      <b-card-text></b-card-text> -->
+
+    <!-- <a href="#" class="card-link">Card link</a>
+      <b-link href="#" class="card-link">Another link</b-link> -->
+  </b-card>
+</template>
+<script>
+export default {
+  props: {
+    game: Object
+  }
+};
+</script>
---
+++ CopaGames/pages/id/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <p>Resultado final</p>
+</template>
+
+<script>
+export default {};
+</script>
