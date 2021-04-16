c624e44fc7418e76aca718f324c4214303e10f45
samuel <samuel.rvg@gmail.com>
2021-04-16 15:24:48 -0400

    Adicionando tests

---
+++ Lambda3.Games/Lambda3.Games.Tests/GameTest.cs
@@ -0,0 +1,129 @@
+using Lambda3.Games.Dominio.Model;
+using Lambda3.Games.Dominio.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace Lambda3.Games.Tests
+{
+    [TestClass]
+    public class GameTest
+    {
+        private List<Game> GamesComNotasIguais { get; set; }
+        private List<Game> GamesComNotasEAnoIguais { get; set; }
+        private List<Game> GamesComNotaMaior { get; set; }
+        private List<Game> ClassificaVencedores { get; set; }
+        private GameService _gameService { get; set; }
+
+        [TestInitialize]
+        public void InitTests()
+        {
+            _gameService = new GameService();
+
+            GamesComNotasIguais = new List<Game>()
+                    {
+                        new Game() { Id = "/nintendo-64/the-legend-of-zelda-ocarina-of-time", Titulo= "The Legend of Zelda: Ocarina of Time (N64)", Nota=98.9, Ano = 1998 },
+                        new Game() { Id = "/playstation/tony-hawks-pro-skater-2", Titulo= "Tony Hawk's Pro Skater 2 (PS)", Nota=98.9, Ano = 2000 },
+                    };
+
+            GamesComNotasEAnoIguais = new List<Game>()
+                    {
+                        new Game() { Id = "/nintendo-64/the-legend-of-zelda-ocarina-of-time", Titulo= "The Legend of Zelda: Ocarina of Time (N64)", Nota=98.9, Ano = 1998 },
+                        new Game() { Id = "/dreamcast/soulcalibur", Titulo= "SoulCalibur (DC)", Nota=98.9, Ano = 1998 },
+                    };
+
+            GamesComNotaMaior = new List<Game>()
+                    {
+                        new Game() { Id = "/nintendo-64/the-legend-of-zelda-ocarina-of-time", Titulo= "The Legend of Zelda: Ocarina of Time (N64)", Nota=98.9, Ano = 1998 },
+                        new Game() { Id = "/playstation/tony-hawks-pro-skater-2", Titulo= "Tony Hawk's Pro Skater 2 (PS)", Nota=99, Ano = 1998 },
+                    };
+
+            ClassificaVencedores = new List<Game>()
+            {
+                new Game() { Id = "/nintendo-64/the-legend-of-zelda-ocarina-of-time", Titulo= "The Legend of Zelda: Ocarina of Time (N64)", Nota=99.9, Ano = 1998 },
+                new Game() { Id = "/dreamcast/soulcalibur", Titulo= "SoulCalibur (DC)", Nota=98.9, Ano = 1999 },
+                new Game() { Id = "/wii/super-mario-galaxy", Titulo= "Super Mario Galaxy (WII)", Nota=97.9, Ano = 2007 },
+                new Game() { Id = "/wii/super-mario-galaxy-2", Titulo= "Super Mario Galaxy 2 (WII)", Nota=97.9, Ano = 2010 },
+
+                new Game() { Id = "/dreamcast/tony-hawks-pro-skater-2", Titulo= "Tony Hawk's Pro Skater 2 (DC)", Nota=97.0, Ano = 2000 },
+                new Game() { Id = "/switch/the-legend-of-zelda-breath-of-the-wild", Titulo= "The Legend of Zelda: Breath of the Wild (Switch)", Nota=97.8, Ano = 2017 },
+                new Game() { Id = "/nintendo-64/perfect-dark", Titulo= "Perfect Dark (N64)", Nota=97.8, Ano = 2000 },
+                new Game() { Id = "/cube/metroid-prime", Titulo= "Metroid Prime (GC)", Nota=97.8, Ano = 2002 },
+            };
+        }
+
+        [TestMethod]
+        public void GamesComNotasIguaisTest()
+        {
+            var mockGameTest = new Game() { Id = "/playstation/tony-hawks-pro-skater-2", Titulo = "Tony Hawk's Pro Skater 2 (PS)", Nota = 98.9, Ano = 2000 };
+
+            var games = _gameService.Classificacao(GamesComNotasIguais);
+
+            Assert.AreEqual(JsonConvert.SerializeObject(mockGameTest), JsonConvert.SerializeObject(games[0]));
+        }
+
+        [TestMethod]
+        public void GamesComNotasEAnoIguaisTest()
+        {
+            var mockGameTest = new Game() { Id = "/dreamcast/soulcalibur", Titulo = "SoulCalibur (DC)", Nota = 98.9, Ano = 1998 };
+
+            var games = _gameService.Classificacao(GamesComNotasEAnoIguais);
+
+            Assert.AreEqual(JsonConvert.SerializeObject(mockGameTest), JsonConvert.SerializeObject(games[0]));
+        }
+
+        [TestMethod]
+        public void GamesComNotaMaiorTest()
+        {
+            var mockGameTest = new Game() { Id = "/playstation/tony-hawks-pro-skater-2", Titulo = "Tony Hawk's Pro Skater 2 (PS)", Nota = 99, Ano = 1998 };
+
+            var games = _gameService.Classificacao(GamesComNotaMaior);
+
+            Assert.AreEqual(JsonConvert.SerializeObject(mockGameTest), JsonConvert.SerializeObject(games[0]));
+        }
+
+        [TestMethod]
+        public void ClassificaVencedoresTest()
+        {
+            var mockGameTest = new List<Game>()
+            {
+                new Game() {Id = "/nintendo-64/the-legend-of-zelda-ocarina-of-time", Titulo= "The Legend of Zelda: Ocarina of Time (N64)", Nota=99.9, Ano = 1998 },
+                new Game() {Id = "/dreamcast/soulcalibur", Titulo= "SoulCalibur (DC)", Nota=98.9, Ano = 1999 }
+            };
+
+            var primeiroGrupo = _gameService.Classificacao(ClassificaVencedores);
+            var segundoGrupo = _gameService.Classificacao(ClassificaVencedores);
+            var finalistas = _gameService.ClassificacaoFinal(ClassificaVencedores);
+
+            Assert.AreEqual(JsonConvert.SerializeObject(mockGameTest), JsonConvert.SerializeObject(finalistas));
+        }
+
+        [TestMethod]
+        public void ClassificaoNaoPodeAceitarListaDeGamesImparesTest()
+        {
+            var mockGameTest = new List<Game>()
+            {
+                new Game() {Id = "/nintendo-64/the-legend-of-zelda-ocarina-of-time", Titulo= "The Legend of Zelda: Ocarina of Time (N64)", Nota=99.9, Ano = 1998 },
+                new Game() {Id = "/nintendo-64/the-legend-of-zelda-ocarina-of-time", Titulo= "The Legend of Zelda: Ocarina of Time (N64)", Nota=99.9, Ano = 1998 },
+                new Game() {Id = "/nintendo-64/the-legend-of-zelda-ocarina-of-time", Titulo= "The Legend of Zelda: Ocarina of Time (N64)", Nota=99.9, Ano = 1998 },
+            };
+
+            Assert.ThrowsException<Exception>(() =>
+            {
+                _gameService.Classificacao(mockGameTest);
+            }, "");
+        }
+
+        [TestMethod]
+        public void ListaDeGamesNaoPodeSerNullTest()
+        {
+            List<Game> mockGameTest = null;
+
+            Assert.ThrowsException<Exception>(() =>
+            {
+                _gameService.Classificacao(mockGameTest);
+            }, "Lista de Games não pode estar vazia");
+        }
+    }
+}
